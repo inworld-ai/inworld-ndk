39d79a6890410fed80bc97f8032982326603e6ef
vezzard <a_vozniuk@inworld.ai>
2023-06-22 15:21:02 +0200

    proto files

--- build-proto.py
+++ build-proto.py
@@ -3,8 +3,8 @@ import subprocess
 
 InworldRepo = "C:/Projects/inworld/inworld/"
 CurDir = os.getcwd() + "/"
-ProtocPath = CurDir + "/build/ThirdParty/grpc/third_party/protobuf/Release/protoc.exe"
-CppPluginPath = CurDir + "/build/ThirdParty/grpc/Release/grpc_cpp_plugin.exe"
+ProtocPath = CurDir + "build/ThirdParty/grpc/third_party/protobuf/Release/protoc.exe"
+CppPluginPath = CurDir + "build/ThirdParty/grpc/Release/grpc_cpp_plugin.exe"
 
 def AddLineToFileStart(Filename, Line):
     print("Adding " + Line + " to " + Filename)
@@ -15,56 +15,51 @@ def AddLineToFileStart(Filename, Line):
             File.seek(0, 0)
             File.write(Line.rstrip('\r\n') + '\n' + Content)
 
-def Generate(File, OutDir):
+def Generate(Path, Filepath):
     print("------------------------------------------")
 
     CmdLine = ProtocPath
     
-    LastSlashIdx = File.rfind('/')
-    CmdLine += " --proto_path=" + File[0:LastSlashIdx]
-      
-    CmdLine += " --proto_path=" + CurDir + "ThirdParty/grpc/third_party/googleapis"
+    CmdLine += " --proto_path=" + CurDir + "ThirdParty/api-common-protos"
     CmdLine += " --proto_path=" + CurDir + "ThirdParty/grpc/third_party/protobuf/src"
-    CmdLine += " --proto_path=" + InworldRepo + "grpc-stub/world-engine/src/main/proto"
+    CmdLine += " --proto_path=" + Path
 
-    OutPath = CurDir + OutDir
+    OutPath = CurDir + "src/proto"
 
     CmdLine += " --grpc_out=" + OutPath
     CmdLine += " --cpp_out=" + OutPath
     CmdLine += " --plugin=protoc-gen-grpc=" + CppPluginPath
 
-    CmdLine += " " + File
+    CmdLine += " " + Path + "/" + Filepath
      
     print(CmdLine)
     subprocess.call(CmdLine, shell=True)
 
-    DotIdx = File.rfind('.')
-    Filename = File[LastSlashIdx+1:DotIdx]
-
-    OutPath2 = OutPath + "/"
-    AddLineToFileStart(OutPath2 + Filename + ".pb.cc", "#include \"ProtoDisableWarning.h\"")
-    AddLineToFileStart(OutPath2 + Filename + ".grpc.pb.cc", "#include \"ProtoDisableWarning.h\"")
+    OutFilename = OutPath + "/" + Filepath
+    OutFilename = OutFilename[0:OutFilename.rfind('.')]
+    AddLineToFileStart(OutFilename + ".pb.cc", "#include \"ProtoDisableWarning.h\"")
+    AddLineToFileStart(OutFilename + ".grpc.pb.cc", "#include \"ProtoDisableWarning.h\"")
     
     print("------------------------------------------")
 
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/world-engine.proto", "src/proto")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/packets.proto", "src/proto")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/voices.proto", "src/proto")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/options.proto", "src/proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "world-engine.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "packets.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "voices.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "options.proto")
 
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/apikeys.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/behavioral_contexts.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/characters.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/errors.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/scenes.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/tokens.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/users.proto", "src/proto/ai/inworld/studio/v1alpha")
-Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto/ai/inworld/studio/v1alpha/workspaces.proto", "src/proto/ai/inworld/studio/v1alpha")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/apikeys.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/behavioral_contexts.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/characters.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/errors.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/scenes.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/tokens.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/users.proto")
+Generate(InworldRepo + "grpc-stub/world-engine/src/main/proto", "ai/inworld/studio/v1alpha/workspaces.proto")
 
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/api/annotations.proto", "src/proto/google/api")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/api/client.proto", "src/proto/google/api")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/api/field_behavior.proto", "src/proto/google/api")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/api/http.proto", "src/proto/google/api")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/api/resource.proto", "src/proto/google/api")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/longrunning/operations.proto", "src/proto/google/longrunning")
-Generate(CurDir + "ThirdParty/grpc/third_party/googleapis/google/rpc/status.proto", "src/proto/google/rpc")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/api/annotations.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/api/client.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/api/field_behavior.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/api/http.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/api/resource.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/longrunning/operations.proto")
+Generate(CurDir + "ThirdParty/grpc/third_party/googleapis", "google/rpc/status.proto")
