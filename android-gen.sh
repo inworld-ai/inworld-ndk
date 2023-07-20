a6e081d9711c1f7781271be9ac98acad931179e2
art-vozniuk <a_vozniuk@inworld.ai>
2023-07-20 15:24:20 +0200

    android github job

--- android-gen.sh
+++ android-gen.sh
@@ -5,5 +5,4 @@ cmake .. -DAEC=False\
  -DCMAKE_SYSTEM_NAME=Android\
  -DCMAKE_SYSTEM_VERSION=31\
  -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a\
- -DCMAKE_ANDROID_NDK=/Users/artem/Library/Android/sdk/ndk/25.1.8937393\
- -DCMAKE_BUILD_TYPE=Debug
+ -DCMAKE_ANDROID_NDK=/Users/runner/Library/Android/sdk/ndk/25.2.9519653
