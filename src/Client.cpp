a08f9c008a083e49030d0bffb1b53fc464d340eb
Div-Inworld <divakar@inworld.ai>
2023-08-10 23:17:50 -0400

    thread await

--- src/Client.cpp
+++ src/Client.cpp
@@ -544,6 +544,8 @@ void Inworld::RunnableAudioDumper::Run()
 
 	while (!_IsDone)
 	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
 		std::string Chunk;
 		while (AudioChuncks.PopFront(Chunk))
 		{
