2e1e56a4010b30a70e4a6702c49e274c1f860004
Artem Vozniuk <a_vozniuk@inworld.ai>
2023-09-11 15:07:31 +0200

    whisper-cpp module and cmake

--- win-gen.bat
+++ win-gen.bat
@@ -1,8 +1,9 @@
 mkdir build
 cd build
-set rootpath=%cd%/..
-cmake .. -DAEC=True -DINWORLD_LOG_SPD=True -DSTT_LOCAL=True -DWITH_MKL=False -DWITH_DNNL=True -DOPENMP_RUNTIME="COMP"^
- -DDNNL_INCLUDE_DIR=%rootpath%/ThirdParty/oneDNN/include^
- -DDNNL_LIBRARY=%rootpath%/ThirdParty/oneDNN/build/src/Release
+::set rootpath=%cd%/..
+cmake .. -DAEC=True -DINWORLD_LOG_SPD=True -DSTT_LOCAL="WHISPERCPP"
+:: -DWITH_MKL=False -DWITH_DNNL=True -DOPENMP_RUNTIME="COMP"^
+:: -DDNNL_INCLUDE_DIR=%rootpath%/ThirdParty/oneDNN/include^
+:: -DDNNL_LIBRARY=%rootpath%/ThirdParty/oneDNN/build/src/Release
 
 pause
