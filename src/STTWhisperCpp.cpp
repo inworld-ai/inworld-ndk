f7a3812b4abced5c81fd09c7cc7519e376c42802
Artem Vozniuk <a_vozniuk@inworld.ai>
2023-09-12 18:53:28 +0200

    whisper wip

--- src/STTWhisperCpp.cpp
+++ src/STTWhisperCpp.cpp
@@ -20,6 +20,8 @@
 #include <vector>
 #include <fstream>
 
+#include "Utils/Log.h"
+
 //  500 -> 00:05.000
 // 6000 -> 01:00.000
 std::string to_timestamp(int64_t t) {
@@ -29,12 +31,12 @@ std::string to_timestamp(int64_t t) {
 	sec = sec - min * 60;
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "%02d:%02d.%03d", (int)min, (int)sec, (int)msec);
+	snInworld::Log(buf, sizeof(buf), "%02d:%02d.%03d", (int)min, (int)sec, (int)msec);
 
 	return std::string(buf);
 }
 
-// command-line parameters
+// parameters
 struct whisper_params {
 	int32_t n_threads = std::min(4, (int32_t)std::thread::hardware_concurrency());
 	int32_t step_ms = 3000;
@@ -90,7 +92,7 @@ bool whisper_params_parse(int argc, char** argv, whisper_params& params) {
 		else if (arg == "-tdrz" || arg == "--tinydiarize") { params.tinydiarize = true; }
 
 		else {
-			fprintf(stderr, "error: unknown argument: %s\n", arg.c_str());
+			Inworld::LogError("error: unknown argument: %s", arg.c_str());
 			whisper_print_usage(argc, argv, params);
 			exit(0);
 		}
@@ -100,39 +102,35 @@ bool whisper_params_parse(int argc, char** argv, whisper_params& params) {
 }
 
 void whisper_print_usage(int /*argc*/, char** argv, const whisper_params& params) {
-	fprintf(stderr, "\n");
-	fprintf(stderr, "usage: %s [options]\n", argv[0]);
-	fprintf(stderr, "\n");
-	fprintf(stderr, "options:\n");
-	fprintf(stderr, "  -h,       --help          [default] show this help message and exit\n");
-	fprintf(stderr, "  -t N,     --threads N     [%-7d] number of threads to use during computation\n", params.n_threads);
-	fprintf(stderr, "            --step N        [%-7d] audio step size in milliseconds\n", params.step_ms);
-	fprintf(stderr, "            --length N      [%-7d] audio length in milliseconds\n", params.length_ms);
-	fprintf(stderr, "            --keep N        [%-7d] audio to keep from previous step in ms\n", params.keep_ms);
-	fprintf(stderr, "  -c ID,    --capture ID    [%-7d] capture device ID\n", params.capture_id);
-	fprintf(stderr, "  -mt N,    --max-tokens N  [%-7d] maximum number of tokens per audio chunk\n", params.max_tokens);
-	fprintf(stderr, "  -ac N,    --audio-ctx N   [%-7d] audio context size (0 - all)\n", params.audio_ctx);
-	fprintf(stderr, "  -vth N,   --vad-thold N   [%-7.2f] voice activity detection threshold\n", params.vad_thold);
-	fprintf(stderr, "  -fth N,   --freq-thold N  [%-7.2f] high-pass frequency cutoff\n", params.freq_thold);
-	fprintf(stderr, "  -su,      --speed-up      [%-7s] speed up audio by x2 (reduced accuracy)\n", params.speed_up ? "true" : "false");
-	fprintf(stderr, "  -tr,      --translate     [%-7s] translate from source language to english\n", params.translate ? "true" : "false");
-	fprintf(stderr, "  -nf,      --no-fallback   [%-7s] do not use temperature fallback while decoding\n", params.no_fallback ? "true" : "false");
-	fprintf(stderr, "  -ps,      --print-special [%-7s] print special tokens\n", params.print_special ? "true" : "false");
-	fprintf(stderr, "  -kc,      --keep-context  [%-7s] keep context between audio chunks\n", params.no_context ? "false" : "true");
-	fprintf(stderr, "  -l LANG,  --language LANG [%-7s] spoken language\n", params.language.c_str());
-	fprintf(stderr, "  -m FNAME, --model FNAME   [%-7s] model path\n", params.model.c_str());
-	fprintf(stderr, "  -f FNAME, --file FNAME    [%-7s] text output file name\n", params.fname_out.c_str());
-	fprintf(stderr, "  -tdrz,     --tinydiarize  [%-7s] enable tinydiarize (requires a tdrz model)\n", params.tinydiarize ? "true" : "false");
-	fprintf(stderr, "\n");
+	Inworld::LogError("");
+	Inworld::LogError("usage: %s [options]", argv[0]);
+	Inworld::LogError("");
+	Inworld::LogError("options:");
+	Inworld::LogError("  -h,       --help          [default] show this help message and exit");
+	Inworld::LogError("  -t N,     --threads N     [%-7d] number of threads to use during computation", params.n_threads);
+	Inworld::LogError("            --step N        [%-7d] audio step size in milliseconds", params.step_ms);
+	Inworld::LogError("            --length N      [%-7d] audio length in milliseconds", params.length_ms);
+	Inworld::LogError("            --keep N        [%-7d] audio to keep from previous step in ms", params.keep_ms);
+	Inworld::LogError("  -c ID,    --capture ID    [%-7d] capture device ID", params.capture_id);
+	Inworld::LogError("  -mt N,    --max-tokens N  [%-7d] maximum number of tokens per audio chunk", params.max_tokens);
+	Inworld::LogError("  -ac N,    --audio-ctx N   [%-7d] audio context size (0 - all)", params.audio_ctx);
+	Inworld::LogError("  -vth N,   --vad-thold N   [%-7.2f] voice activity detection threshold", params.vad_thold);
+	Inworld::LogError("  -fth N,   --freq-thold N  [%-7.2f] high-pass frequency cutoff", params.freq_thold);
+	Inworld::LogError("  -su,      --speed-up      [%-7s] speed up audio by x2 (reduced accuracy)", params.speed_up ? "true" : "false");
+	Inworld::LogError("  -tr,      --translate     [%-7s] translate from source language to english", params.translate ? "true" : "false");
+	Inworld::LogError("  -nf,      --no-fallback   [%-7s] do not use temperature fallback while decoding", params.no_fallback ? "true" : "false");
+	Inworld::LogError("  -ps,      --print-special [%-7s] print special tokens", params.print_special ? "true" : "false");
+	Inworld::LogError("  -kc,      --keep-context  [%-7s] keep context between audio chunks", params.no_context ? "false" : "true");
+	Inworld::LogError("  -l LANG,  --language LANG [%-7s] spoken language", params.language.c_str());
+	Inworld::LogError("  -m FNAME, --model FNAME   [%-7s] model path", params.model.c_str());
+	Inworld::LogError("  -f FNAME, --file FNAME    [%-7s] text output file name", params.fname_out.c_str());
+	Inworld::LogError("  -tdrz,     --tinydiarize  [%-7s] enable tinydiarize (requires a tdrz model)", params.tinydiarize ? "true" : "false");
+	Inworld::LogError("");
 }
 
-void Inworld::STT::Start() {
+void Inworld::RunnableSTT::Run()
+{
 	whisper_params params;
-
-	/*if (whisper_params_parse(argc, argv, params) == false) {
-		return 1;
-	}*/
-
 	params.keep_ms = std::min(params.keep_ms, params.step_ms);
 	params.length_ms = std::max(params.length_ms, params.step_ms);
 
@@ -149,21 +147,9 @@ void Inworld::STT::Start() {
 	params.no_context |= use_vad;
 	params.max_tokens = 0;
 
-	// init audio
-
-	/*audio_async audio(params.length_ms);
-	if (!audio.init(params.capture_id, WHISPER_SAMPLE_RATE)) {
-		fprintf(stderr, "%s: audio.init() failed!\n", __func__);
-		return 1;
-	}
-
-	audio.resume();*/
-
 	// whisper init
-
 	if (params.language != "auto" && whisper_lang_id(params.language.c_str()) == -1) {
-		fprintf(stderr, "error: unknown language '%s'\n", params.language.c_str());
-		//whisper_print_usage(argc, argv, params);
+		Inworld::LogError("error: unknown language '%s'", params.language.c_str());
 		exit(0);
 	}
 
@@ -177,15 +163,14 @@ void Inworld::STT::Start() {
 
 	// print some info about the processing
 	{
-		fprintf(stderr, "\n");
 		if (!whisper_is_multilingual(ctx)) {
 			if (params.language != "en" || params.translate) {
 				params.language = "en";
 				params.translate = false;
-				fprintf(stderr, "%s: WARNING: model is not multilingual, ignoring language and translation options\n", __func__);
+				Inworld::LogError("%s: WARNING: model is not multilingual, ignoring language and translation options", __func__);
 			}
 		}
-		fprintf(stderr, "%s: processing %d samples (step = %.1f sec / len = %.1f sec / keep = %.1f sec), %d threads, lang = %s, task = %s, timestamps = %d ...\n",
+		Inworld::LogError("%s: processing %d samples (step = %.1f sec / len = %.1f sec / keep = %.1f sec), %d threads, lang = %s, task = %s, timestamps = %d ...",
 			__func__,
 			n_samples_step,
 			float(n_samples_step) / WHISPER_SAMPLE_RATE,
@@ -197,30 +182,16 @@ void Inworld::STT::Start() {
 			params.no_timestamps ? 0 : 1);
 
 		if (!use_vad) {
-			fprintf(stderr, "%s: n_new_line = %d, no_context = %d\n", __func__, n_new_line, params.no_context);
+			Inworld::Log("%s: n_new_line = %d, no_context = %d", __func__, n_new_line, params.no_context);
 		}
 		else {
-			fprintf(stderr, "%s: using VAD, will transcribe on speech activity\n", __func__);
+			Inworld::Log("%s: using VAD, will transcribe on speech activity", __func__);
 		}
-
-		fprintf(stderr, "\n");
 	}
 
 	int n_iter = 0;
 
 	bool is_running = true;
-
-	std::ofstream fout;
-	if (params.fname_out.length() > 0) {
-		fout.open(params.fname_out);
-		if (!fout.is_open()) {
-			fprintf(stderr, "%s: failed to open output file '%s'!\n", __func__, params.fname_out.c_str());
-			return;
-		}
-	}
-
-	printf("[Start speaking]");
-	fflush(stdout);
 
 	auto t_last = std::chrono::high_resolution_clock::now();
 	const auto t_start = t_last;
@@ -241,7 +212,7 @@ void Inworld::STT::Start() {
 				audio.get(params.step_ms, pcmf32_new);
 
 				if ((int)pcmf32_new.size() > 2 * n_samples_step) {
-					fprintf(stderr, "\n\n%s: WARNING: cannot process audio fast enough, dropping audio ...\n\n", __func__);
+					Inworld::LogWarning("%s: WARNING: cannot process audio fast enough, dropping audio ...", __func__);
 					audio.clear();
 					continue;
 				}
@@ -259,7 +230,7 @@ void Inworld::STT::Start() {
 			// take up to params.length_ms audio from previous iteration
 			const int n_samples_take = std::min((int)pcmf32_old.size(), std::max(0, n_samples_keep + n_samples_len - n_samples_new));
 
-			//printf("processing: take = %d, new = %d, old = %d\n", n_samples_take, n_samples_new, (int) pcmf32_old.size());
+			//Inworld::Log("processing: take = %d, new = %d, old = %d", n_samples_take, n_samples_new, (int) pcmf32_old.size());
 
 			pcmf32.resize(n_samples_new + n_samples_take);
 
@@ -276,7 +247,7 @@ void Inworld::STT::Start() {
 			const auto t_diff = std::chrono::duration_cast<std::chrono::milliseconds>(t_now - t_last).count();
 
 			if (t_diff < 2000) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				//std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 				continue;
 			}
@@ -322,27 +293,27 @@ void Inworld::STT::Start() {
 			wparams.prompt_n_tokens = params.no_context ? 0 : prompt_tokens.size();
 
 			if (whisper_full(ctx, wparams, pcmf32.data(), pcmf32.size()) != 0) {
-				fprintf(stderr, "%s: failed to process audio\n", argv[0]);
+				Inworld::LogError("%s: failed to process audio", argv[0]);
 				return 6;
 			}
 
 			// print result;
 			{
 				if (!use_vad) {
-					printf("\33[2K\r");
+					Inworld::Log("\33[2K\r");
 
 					// print long empty line to clear the previous line
-					printf("%s", std::string(100, ' ').c_str());
+					Inworld::Log("%s", std::string(100, ' ').c_str());
 
-					printf("\33[2K\r");
+					Inworld::Log("\33[2K\r");
 				}
 				else {
 					const int64_t t1 = (t_last - t_start).count() / 1000000;
 					const int64_t t0 = std::max(0.0, t1 - pcmf32.size() * 1000.0 / WHISPER_SAMPLE_RATE);
 
-					printf("\n");
-					printf("### Transcription %d START | t0 = %d ms | t1 = %d ms\n", n_iter, (int)t0, (int)t1);
-					printf("\n");
+					Inworld::Log("");
+					Inworld::Log("### Transcription %d START | t0 = %d ms | t1 = %d ms", n_iter, (int)t0, (int)t1);
+					Inworld::Log("");
 				}
 
 				const int n_segments = whisper_full_n_segments(ctx);
@@ -350,12 +321,7 @@ void Inworld::STT::Start() {
 					const char* text = whisper_full_get_segment_text(ctx, i);
 
 					if (params.no_timestamps) {
-						printf("%s", text);
-						fflush(stdout);
-
-						if (params.fname_out.length() > 0) {
-							fout << text;
-						}
+						Inworld::Log("%s", text);
 					}
 					else {
 						const int64_t t0 = whisper_full_get_segment_t0(ctx, i);
@@ -367,31 +333,21 @@ void Inworld::STT::Start() {
 							output += " [SPEAKER_TURN]";
 						}
 
-						output += "\n";
+						output += "";
 
-						printf("%s", output.c_str());
-						fflush(stdout);
-
-						if (params.fname_out.length() > 0) {
-							fout << output;
-						}
+						Inworld::Log("%s", output.c_str());
 					}
 				}
 
-				if (params.fname_out.length() > 0) {
-					fout << std::endl;
-				}
-
 				if (use_vad) {
-					printf("\n");
-					printf("### Transcription %d END\n", n_iter);
+					Inworld::Log("### Transcription %d END", n_iter);
 				}
 			}
 
 			++n_iter;
 
 			if (!use_vad && (n_iter % n_new_line) == 0) {
-				printf("\n");
+				Inworld::Log("");
 
 				// keep part of the audio for next iteration to try to mitigate word boundary issues
 				pcmf32_old = std::vector<float>(pcmf32.end() - n_samples_keep, pcmf32.end());
@@ -417,13 +373,6 @@ void Inworld::STT::Start() {
 
 	whisper_print_timings(ctx);
 	whisper_free(ctx);
-
-	return 0;
-}
-
-void Inworld::RunnableSTT::Run()
-{
-
 }
 
 
