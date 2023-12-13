3b5e0adf6b84bb9c70f35daa7bbf3b5aaaa6faa5
Artem Vozniuk <a_vozniuk@inworld.ai>
2023-12-13 16:18:24 +0100

    add aec files

---
+++ inworld-aec/src/AECInterop.cpp
@@ -0,0 +1,42 @@
+﻿#include "AECInterop.h"
+
+#include <iostream>
+
+void* WebRtcAec3_Create(int32_t sample_rate_hz)
+{
+	g_pAECHandle = new webrtc::AECHandle();
+	return g_pAECHandle;
+}
+
+void WebRtcAec3_Free(void *handle)
+{
+	if (g_pAECHandle)
+	{
+		delete g_pAECHandle;
+		g_pAECHandle = nullptr;
+	}
+}
+int32_t WebRtcAec3_BufferFarend(void* handle, const int16_t* farend)
+{
+	// In case handle is not g_pAECHandle.
+	
+	if (g_pAECHandle)
+	{
+		g_pAECHandle->SampleOutput(farend);
+		return 0;
+	}
+	return -1;
+}
+int32_t WebRtcAec3_Process(void* handle, const int16_t* nearend, int16_t* out)
+{
+	// In case handle is not g_pAECHandle.
+	if (g_pAECHandle)
+	{
+		g_pAECHandle->SampleInput(nearend);
+		memcpy(out, g_pAECHandle->GetFilteredData(), webrtc::AECHandle::kBytesPerFrame);
+		return 0;
+	}
+	return -1;
+}
+
+
