da117aea2616ec0a3fa650cc7910dc14e8980168
Artem Vozniuk <a_vozniuk@inworld.ai>
2024-01-23 17:24:43 +0400

    gen protos

--- src/proto/ai/inworld/studio/v1alpha/workspaces.pb.h
+++ src/proto/ai/inworld/studio/v1alpha/workspaces.pb.h
@@ -41,8 +41,8 @@
 #include <google/protobuf/empty.pb.h>
 #include <google/protobuf/field_mask.pb.h>
 #include <google/protobuf/timestamp.pb.h>
-#include "options.pb.h"
-#include "language_codes.pb.h"
+#include "ai/inworld/options/options.pb.h"
+#include "ai/inworld/language_codes/language_codes.pb.h"
 // @@protoc_insertion_point(includes)
 #include <google/protobuf/port_def.inc>
 #define PROTOBUF_INTERNAL_EXPORT_ai_2finworld_2fstudio_2fv1alpha_2fworkspaces_2eproto
