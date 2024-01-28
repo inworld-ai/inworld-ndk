fa6b9f2e290de4396374349f9b2e44fbb4f087de
art-vozniuk <a_vozniuk@inworld.ai>
2024-01-28 19:12:56 +0400

    fix header

--- inworld-aec/src/AECInterop.cpp
+++ inworld-aec/src/AECInterop.cpp
@@ -1,4 +1,11 @@
-﻿#include "AECInterop.h"
+﻿/**
+* Copyright 2022 Theai, Inc. (DBA Inworld)
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ */
+
+#include "AECInterop.h"
 #include "AECHandle.h"
 
 #include <iostream>
