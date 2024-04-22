3c89156108c3a2525da4344d06003fdee918f9dc
Artem Vozniuk <a_vozniuk@inworld.ai>
2024-04-22 15:18:23 +0200

    vad wip

--- win-gen.bat
+++ win-gen.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -DAEC=True -DINWORLD_LOG_SPD=True -DINWORLD_SHARED=False -DINWORLD_CPPCHECK=True
+cmake .. -DAEC=True -DINWORLD_LOG_SPD=True -DINWORLD_SHARED=False -DINWORLD_CPPCHECK=True -DINWORLD_VAD=True
