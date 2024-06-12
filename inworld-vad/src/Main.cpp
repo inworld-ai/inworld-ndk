956006cfd9a9e759d5dd94ecdf7f4760a2974018
Artem Vozniuk <a_vozniuk@inworld.ai>
2024-06-12 15:42:05 +0200

    fix header

--- inworld-vad/src/Main.cpp
+++ inworld-vad/src/Main.cpp
@@ -1,3 +1,10 @@
+/**
+* Copyright 2023-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ */
+
 #include "InworldVAD.h"
 #include <iostream>
 
