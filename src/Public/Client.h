4eed0beb1383312198d61e390955525d03dc82b1
Matt Carey <MattCarey@gmx.com>
2024-07-10 10:57:44 -0400

    Pause to not discard packets
    Currently PauseClient calls StopClientStream(); which cancels the session. Any pending packets are lost.
    Instead, Pause should mark the stream as finished and not forward any events. Any recv'd packets are simply queued, and no packets are allowed to write.

--- src/Public/Client.h
+++ src/Public/Client.h
@@ -238,6 +238,8 @@ namespace Inworld
 		void PushPacket(std::shared_ptr<Inworld::Packet> Packet);
 
 		void StartClientStream();
+		void PauseClientStream();
+		void ResumeClientStream();
 		void StopClientStream();
 		void SetConnectionState(ConnectionState State);
 
