e68e46ac5cef8eff41bb4a2feba35441042eaf69
Matt-Carey <mcarey@inworld.ai>
2024-10-22 16:51:12 -0400

    Update Client.cpp

--- src/Private/Client.cpp
+++ src/Private/Client.cpp
@@ -753,7 +753,7 @@ void Inworld::Client::PauseClientStream()
 
 void Inworld::Client::ResumeClientStream()
 {
-	if (_ConnectionState == ConnectionState::Disconnected)
+	if (!_Service->Stream())
 	{
 		StartClientStream();
 	}
