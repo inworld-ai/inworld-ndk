85fef78b5453f9832658f4282f81218d8ae125ed
Matt-Carey <mcarey@inworld.ai>
2024-10-22 22:28:00 -0400

    upgrade android ndk

--- android-gen.sh
+++ android-gen.sh
@@ -6,4 +6,4 @@ cmake .. -DAEC=False\
  -DCMAKE_SYSTEM_NAME=Android\
  -DCMAKE_SYSTEM_VERSION=31\
  -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a\
- -DCMAKE_ANDROID_NDK=/Users/runner/Library/Android/sdk/ndk/25.2.9519653
+ -DCMAKE_ANDROID_NDK=/Users/runner/Library/Android/sdk/ndk/26.3.11579264
